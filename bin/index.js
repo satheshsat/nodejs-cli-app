a49e92ba56236b0d1fc9b901308b72ccd1f63db2
Sathesh kumar <satheshs.sat@gmail.com>
2025-04-08 10:10:42 +0530

    Add sqlite memory

--- bin/index.js
+++ bin/index.js
@@ -10,8 +10,26 @@ import { runInquirerListExample } from "../src/commands/inquirer-list-example.js
 import { runOraExample } from "../src/commands/ora-example.js";
 import { ageCalc } from "../src/commands/age-calculation.js";
 import open, { openApp, apps } from "open";
+import sqlite3 from 'sqlite3'
+// let db = new sqlite3.Database(':memory:'); // Or provide a file path for persistent storage
+let db = new sqlite3.Database('test.sqlite'); // Or provide a file path for persistent storage
 
 const examples = {
+  "sqlite": () => {
+    db.serialize(function() {
+      db.run("CREATE TABLE user (name TEXT)");
+    
+      const stmt = db.prepare("INSERT INTO user VALUES (?)");
+      stmt.run("ChatGPT");
+      stmt.finalize();
+    
+      db.each("SELECT rowid AS id, name FROM user", function(err, row) {
+        console.log(row.id + ": " + row.name);
+      });
+    });
+    
+    db.close();
+  },
   "Open Google": () => {
     open("https://www.google.com");
   },
